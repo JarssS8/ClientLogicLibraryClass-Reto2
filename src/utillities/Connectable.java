3a3c91753fd2758c25a688764e55bd68f09f47f8
JarssS8 <adriancgs@gmail.com>
2019-10-22 21:19:21 +0200

    Finished structure of the utility class

---
+++ src/utillities/Connectable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utillities;
+
+import utillities.User;
+
+/**
+ * This is the interface of both sides, client and server. 
+ * @author Adrian
+ */
+public interface Connectable {
+    public void logIn(User user);
+    public void signUp(User user);
+    public void logOut(User user);
+    
+}
